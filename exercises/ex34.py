f6f65c1c7a22cdc00a7e5130c472ca945514c945
Taylor Hoff <taylor@rexchange.com>
2018-01-24 18:40:33 -0800

    Exercise 34 DONE!

---
+++ exercises/ex34.py
@@ -0,0 +1,25 @@
+#Exercise 34 - Accessing Elements of Lists 
+
+def main():
+    animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+    print("Here are the animals and their ordinal numbers:")
+    print(f"""Animals (Ordinal):
+{animals[0]} = First 
+{animals[1]} = Second
+{animals[2]} = Third
+{animals[3]} = Fourth
+{animals[4]} = Fifth
+{animals[5]} = Sixth
+""")
+    print("Here are the animals and their cardinal numbers:")
+    print(f"""Animals (Cardinal):
+{animals[0]} = 0
+{animals[1]} = 1
+{animals[2]} = 2
+{animals[3]} = 3
+{animals[4]} = 4
+{animals[5]} = 5
+""")
+
+main()
+
